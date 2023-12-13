d8c628652f36e174f1448ba7770113c9d4b2600d
Debbie Olorunisola <dolorunisola@gmail.com>
2023-12-13 17:40:15 -0500

    Adding new exploratory graphs and committing before doing some overhauls to analysis.

--- analysis.R
+++ analysis.R
@@ -30,7 +30,7 @@ splitobj <- initial_split(df, prop = 4/5) #initial object, 80/20 split
 train <- training(splitobj)
 test <- testing(splitobj)
 
-#1: The Paper's Linear Models----
+#TOP 1: The Paper's Linear Models----
 #1: 7-predictor logistic algorithm assessment (uses full dataset)
 train_sub <- train |>
   select(two_year_recid, age, sex, priors_count, juv_fel_count, juv_misd_count, charge_degree, charge_id) #fixes contrasts error
@@ -56,6 +56,7 @@ y_hat <- as.numeric(y_hat > 0.5)
 mean(y_hat == df$two_year_recid[df$id %in% test$id]) #accuracy
 
 roc(df$two_year_recid[df$id %in% train$id] ~ two_pred$fitted.values, plot = TRUE, print.auc = TRUE)
+#bootstrap standard errors
 
 #3: My 7-predictor model (different aggregation)
 train_sub <- train |>
@@ -69,3 +70,37 @@ y_hat <- as.numeric(y_hat > 0.5)
 mean(y_hat == df$two_year_recid[df$id %in% test$id]) #accuracy
 
 roc(df$two_year_recid[df$id %in% train$id] ~ my_sev$fitted.values, plot = TRUE, print.auc = TRUE)
+
+#TOP 2: Given race, white people have a negative correlation with decile score, while Black people have a uniform one----
+dec_white <- df |>
+  filter(race == 1) |>
+  group_by(compas_decile_score) |>
+  count(race, compas_decile_score)
+chisq.test(dec_white$n) #probability uniform
+chisq.test(dec_white$n[1:9]) #probability uniform without 10th decile
+
+dec_black <- df |>
+  filter(race == 2) |>
+  group_by(compas_decile_score) |>
+  count(compas_decile_score)
+chisq.test(dec_black$n) #probability uniform dist
+chisq.test(dec_black$n[1:9]) #probability uniform without 10th decile
+
+#TOP 3: Finding vars that have a similarly poor prediction w race----
+lm(compas_decile_score ~ drug_type*race, df |> filter(race %in% 1:2))
+lm(compas_correct ~ drug_type, df)
+lm(compas_correct ~ drug_type, df |> filter(race %in% 1:2)) #also run as OLS
+
+correct_drug <- glm(compas_correct ~ decile*drug_type |> filter(drug_type == "Cocaine"), data=train, family="binomial")
+correct_drug_sub <- glm(compas_correct ~ drug_type, data=train |> filter(race %in% 1:2), family="binomial")
+#exponentiate coeffs bc log odds
+correct_drug_race <- glm(compas_correct ~ drug_type*race, data=train |> filter(race %in% 1:2), family="binomial")
+
+y_hat_drug <- predict(correct_drug, newdata=train, type="response")
+predict(correct_drug_sub, newdata=test |> filter(race %in% 1:2), type="response")
+predict(correct_drug_race, newdata=test |> filter(race %in% 1:2), type="response")
+
+plot(train, y_hat_drug, pch = 16, xlab = "dat", ylab = "VS")
+lines(xdisp, ydisp)
+
+#since i know that priors can be used to estimate correct guess, can instrument for...?
--- data_viz.R
+++ data_viz.R
@@ -16,10 +16,11 @@ df |>
   filter(n >= 20) |>
   ggplot(aes(x = fct_reorder(aggregated, n), y = n)) +
   geom_col() +
-  labs(main = "Distribution of Charges Using New Label Aggregates",
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) + #rotate label 
+  labs(title = "Distribution of Charges Using New Label Aggregates",
        x = "Charge Type",
-       y = "Count") +
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) #rotate label
+       y = "Count")
+  
 
 #visualizing the dataset----
 df |>
@@ -41,8 +42,12 @@ df |>
   group_by(drug_type) |>
   count(drug_type, compas_decile_score, .drop = FALSE) |>
   ggplot(aes(x = compas_decile_score, y = n, fill = drug_type)) + 
-  geom_bar(position="dodge", stat="identity") #can check if theres a uniform distribution here... GOF test
+  geom_bar(position="dodge", stat="identity") + #can check if theres a uniform distribution here... GOF test
 #NAs from Trafficking unspecified drug
+  labs(title = "Distribution of Drug Usage",
+       x = "COMPAS Decile Score",
+       y = "Count",
+       fill = "Drug Type")
 
 
 df |>
@@ -53,10 +58,13 @@ df |>
   filter(race %in% 1:2) |>
   ggplot(aes(x = drug_type, y = n, fill = race)) + 
   geom_bar(position="dodge", stat="identity") +
-  labs(main = "Bar Plot Comparing Race and Drug Type",
+  labs(title = "Bar Plot Comparing Race and Drug Type",
        x = "Charge Type",
-       y = "Count") +
-  theme(title = element_text(hjust = 0.5), #not showing title??
+       y = "Count",
+       fill = "Race") +
+  scale_fill_discrete("Race", 
+                      labels=c("White", "Black")) +
+  theme(title = element_text(hjust = 0.5), 
         axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
 df |>
@@ -64,4 +72,35 @@ df |>
   group_by(compas_decile_score) |>
   count(race, compas_decile_score) |>
   ggplot(aes(x = compas_decile_score, y = n, fill = race)) + 
-  geom_bar(position="dodge", stat="identity")
+  geom_bar(position="dodge", stat="identity") +
+  labs(title = "Bar Plot Comparing Race and Decile Score",
+       x = "COMPAS Decile Score",
+       y = "Count",
+       fill = "Race") +
+  scale_fill_discrete("Race", 
+                      labels=c("White", "Black")) +
+  scale_x_discrete(limits = factor(1:10))
+
+#compas guess and correct
+df |>
+  filter(race %in% 1:2) |>
+  group_by(race) |>
+  ggplot(aes(x = priors_count, y = compas_guess, color = race)) +
+  geom_point(position = position_jitter(width = 0.05, height = 0.05), alpha = 0.5)
+
+df |>
+  filter(race %in% 1:2) |>
+  group_by(race) |>
+  ggplot(aes(x = priors_count, y = compas_correct, color = race)) +
+  geom_point(position = position_jitter(width = 0.05, height = 0.05), alpha = 0.5)
+
+# you see that, btwn the graphs, theres an increase in blue at the bottom for the correct graph, showing how Black people are more likely to be misguessed
+
+#FOCUS: i want to figure out a relationship btwn priors/age and another variable...
+df |>
+  filter(!is.na(drug_interaction)) |>
+  group_by(drug_interaction) |>
+  count(drug_interaction, race, .drop = FALSE) |>
+  filter(race %in% 1:2) #not much difference here~
+
+
